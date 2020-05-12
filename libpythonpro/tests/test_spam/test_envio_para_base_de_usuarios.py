f732a7cb5889f3d9b553aee8e2cb7d9ee6faebaa
ivansilva86 <63621288+ivansilva86@users.noreply.github.com>
2020-05-12 14:55:14 -0300

    test_usuarios

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -2,7 +2,7 @@ from libpythonpro.spam.enviador_de_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
 
 
-def test_envio_de_spam():
+def test_envio_de_spam(sessao):
     enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
     enviador_de_spam.enviar_emails(
         'ivan.paulosilva@gmail.com',
