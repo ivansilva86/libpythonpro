8c3d2b28c6af786b1a33d012bdd77de34c609ba5
ivansilva86 <63621288+ivansilva86@users.noreply.github.com>
2020-05-12 15:56:27 -0300

    test_usuarios

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,11 +1,30 @@
+import pytest
+
 from libpythonpro.spam.enviador_de_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
+from libpythonpro.spam.modelos import Usuario
 
 
-def test_envio_de_spam(sessao):
-    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Ivan', email='ivan.paulosilva@gmail.com'),
+            Usuario(nome='Luciano', email='luciano@gmail.com')
+        ],
+        [
+            Usuario(nome='Luciano', email='luciano@gmail.com')
+        ]
+    ]
+)
+def test_qtde_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = Enviador()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'ivan.paulosilva@gmail.com',
         'Curso Python-Pro',
         'Confira os módulos fantásticos'
     )
+    assert len(usuarios) == enviador.qtd_de_emails_enviados
--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -2,13 +2,16 @@ from libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Ivan')
+    usuario = Usuario(nome='Ivan', email='ivan.paulosilva@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuario(sessao):
-    usuarios = [Usuario(nome='Ivan'), Usuario(nome='Luciano')]
+    usuarios = [
+        Usuario(nome='Ivan', email='ivan.paulosilva@gmail.com'),
+        Usuario(nome='Luciano', email='luciano@gmail.com')
+    ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
